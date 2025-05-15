32c9ec069aa93469610ceb2454fb25c8240ab044
Shivika garg <shivikagarg91@gmail.com>
2025-05-15 19:42:49 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 connectDB(); // connect to MongoDB
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://mentoroid.vercel.app'],
+  origin: ['http://localhost:3000', 'https://mentoroid.vercel.app', 'https://www.mentroid.live'],
   credentials: true
 }));
 
